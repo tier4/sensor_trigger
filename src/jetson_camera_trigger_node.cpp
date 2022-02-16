5c4853584520905fd63167ace10668a113414ce7
davidw <david.wong@tier4.jp>
2022-02-16 16:27:48 +0900

    Change header blocks

--- src/jetson_camera_trigger_node.cpp
+++ src/jetson_camera_trigger_node.cpp
@@ -14,22 +14,6 @@
  * limitations under the License.
  */
 
-/*
- * Copyright 2015-2022 Autoware Foundation. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 #include <jetson_camera_trigger/jetson_gpio.h>
 #include <jetson_camera_trigger/jetson_camera_trigger_node.hpp>
 #include <std_msgs/Time.h>
@@ -41,7 +25,7 @@ JetsonCameraTriggerNode::JetsonCameraTriggerNode()
   private_nh_.param("phase", phase_, double(0));
   private_nh_.param("gpio", gpio_, 0);
 
-  if (gpio_ == 0)
+  if (gpio_ <= 0)
   {
     ROS_WARN_STREAM("No valid trigger GPIO specified. Not using triggering on " << gpio_ << ".");
     use_triggering_ = false;
